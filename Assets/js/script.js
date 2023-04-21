8664039c43c54ba23e0a55d518535f701d1373bf
unknown <barronpeterson56@gmail.com>
2023-04-20 21:33:50 -0500

    add read me and comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,16 @@
 $(function () {
+  //pulls the current date and the hour in military time
   today = dayjs(new Date).format('dddd, MMMM D');
   currentTime = parseInt(dayjs(new Date).format('H'));
 $("#currentDay").text(today);
 
 console.log(currentTime);
-
+//defines variables
 var workHours = 9;
 var endWork = 18;
 
 console.log(workHours);
-
+//goes through each hour and checks what state it is in and gives it the corresponding class
 while(workHours < endWork){
 var wh = workHours;
 console.log(wh);
@@ -30,14 +31,14 @@ console.log(workHours);
 
 workHours++;
 }
-
+//saves user input to local storage
 $(".saveBtn").on("click", function(){
     var userInput = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
     localStorage.setItem(time,userInput);
 })
-
+//recalls the local storage to the correct location
 $('#9 .description').val(localStorage.getItem("9"));
 $('#10 .description').val(localStorage.getItem("10"));
 $('#11 .description').val(localStorage.getItem("11"));
